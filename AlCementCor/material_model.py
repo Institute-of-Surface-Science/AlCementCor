2cb26af360812e06fbe661abae2ec05d6185b580
Sven Berger <berger.sven@gmail.com>
2023-08-18 16:26:40 +0200

    improve class definition

--- AlCementCor/material_model.py
+++ AlCementCor/material_model.py
@@ -3,11 +3,48 @@ import fenics as fe
 
 
 class LinearElastoPlasticModel:
-    def __init__(self, simulation_config, mesh):
+    def __init__(self, simulation_config: 'SimulationConfig', mesh: 'MeshType'):
         """Initialize the model with given configurations and mesh."""
         self._simulation_config = simulation_config
         self._mesh = mesh
 
+        # Function spaces
+        self.deg_stress: int = None
+        self.V = None
+        self.DG = None
+        self.W = None
+        self.W0 = None
+        self.P0 = None
+
+        # Displacement functions
+        self.u = None
+        self.du = None
+        self.Du = None
+
+        # Stress functions
+        self.sig = None
+        self.sig_old = None
+        self.n_elas = None
+
+        # Other functions and parameters
+        self.beta = None
+        self.p = None
+        self.sig_hyd = None
+        self.sig_0_local = None
+        self.mu_local = None
+        self.lmbda_local = None
+        self.C_linear_h_local = None
+        self.sig_hyd_avg = None
+        self.sig_0_test = None
+        self.lmbda_test = None
+        self.metadata = None
+        self.dxm = None
+        self.v = None
+        self.u_ = None
+
+        self._setup()
+
+    def _setup(self):
         self._setup_function_spaces()
         self._setup_displacement_functions()
         self._setup_stress_functions()
@@ -46,5 +83,13 @@ class LinearElastoPlasticModel:
         self.dxm = ufl.dx(metadata=self.metadata)
         self.v = fe.TrialFunction(self.V)
         self.u_ = fe.TestFunction(self.V)
+
+    @property
+    def mesh(self) -> 'MeshType':
+        return self._mesh
+
+    @property
+    def simulation_config(self) -> 'SimulationConfig':
+        return self._simulation_config
 
 
